757bdb14d87cc34bc11bd923168eca77d8d58d3b
Alexander Ruziska <ziskapants@gmail.com>
2025-01-17 11:12:55 -0600

    got numbers to post to console

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,15 +2,57 @@ function onReady() {
   console.log("JavaScript is loaded!")
 }
 
+function formHandler(event) {
+  event.preventDefault();
+  console.log('new guesses');
 
-function fetchGuesses() {
-  //clear old data
-  const contentDiv = document.querySelector('#playerTableBody');
-  contentDiv.innerHTML = '';
+  const newPlayer = {
+    player1: document.getElementById('Ismail-name').value,
+    player2: document.getElementById('Alex-name').value,
+    player3: document.getElementById('Alecia-name').value,
+  };
+
+  console.log('newPlayer', newPlayer);
+
+  //use axios to post to server, need the newArtist obj and where to go/ post
+ // addGuess.push(newPlayer);
+
+  //clear form fields look into the form.reset() method??
+//  document.getElementById('artist-form-id').reset();
+}
+
+function addGuess(guess) {
+  axios
+    .post('/playerdata', guess)
+    .then((response) => {
+      console.log('Guesses submitted');
+      //refresh the data!!!!
+      //fetchArtist();
+    })
+    .catch((error) => {
+      console.error('whoops! something went wrong with the guess post');
+    });
+}
+function onSubmit() {
+//   //clear old data
+//   const contentDiv = document.querySelector('#playerTableBody');
+//   contentDiv.innerHTML = '';
+
+//   //alecia's opinion
+//   event.preventDefault();
+//   const newGuesses = {
+//     playerOne: document.getElementById("Ismail-name").value,
+//     playerTwo: document.getElementById("Alex-name").value,
+//     playerThree: document.getElementById('Alecia-name').value,
+//   };
+//   //end alecia's rambling
+
   axios({
-    method: 'GET',
+    method: 'POST',
     url: '/playerdata',
   })
+
+
     .then(function (response) {
       // Code that will run on successful response
       // from the server.
@@ -34,6 +76,8 @@ function fetchGuesses() {
       alert('Something bad happened! Check the console for more details.');
     });
 }
+
+
 
 
 onReady()
