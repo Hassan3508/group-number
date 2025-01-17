878256a4df2368829ad7927573d8628657978a4f
Alexander Ruziska <ziskapants@gmail.com>
2025-01-17 11:42:10 -0600

    updated the client js file

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,77 +1,61 @@
 function onReady() {
   console.log("JavaScript is loaded!")
+
 }
+
 
 function formHandler(event) {
   event.preventDefault();
-  console.log('new guesses');
-
+  console.log('new guesses', );
   const newPlayer = {
-    player1: document.getElementById('Ismail-name').value,
-    player2: document.getElementById('Alex-name').value,
-    player3: document.getElementById('Alecia-name').value,
+    answerOne: document.getElementById('Ismail-name').value,
+    answerTwo: document.getElementById('Alex-name').value,
+    answerThree: document.getElementById('Alecia-name').value,
+
   };
-
+  document.getElementById('form').reset();
+  
+addGuess(newPlayer);
   console.log('newPlayer', newPlayer);
-
-  //use axios to post to server, need the newArtist obj and where to go/ post
- // addGuess.push(newPlayer);
-
-  //clear form fields look into the form.reset() method??
-//  document.getElementById('artist-form-id').reset();
 }
+
 
 function addGuess(guess) {
   axios
     .post('/playerdata', guess)
     .then((response) => {
       console.log('Guesses submitted');
-      //refresh the data!!!!
-      //fetchArtist();
+      fetchGuess();
     })
     .catch((error) => {
       console.error('whoops! something went wrong with the guess post');
     });
+    
 }
-function onSubmit() {
-//   //clear old data
-//   const contentDiv = document.querySelector('#playerTableBody');
-//   contentDiv.innerHTML = '';
 
-//   //alecia's opinion
-//   event.preventDefault();
-//   const newGuesses = {
-//     playerOne: document.getElementById("Ismail-name").value,
-//     playerTwo: document.getElementById("Alex-name").value,
-//     playerThree: document.getElementById('Alecia-name').value,
-//   };
-//   //end alecia's rambling
 
+function fetchGuess() {
+  const contentDiv = document.querySelector('#playerTableBody');
+  contentDiv.innerHTML = '';
   axios({
-    method: 'POST',
+    method: 'GET',
     url: '/playerdata',
   })
-
-
     .then(function (response) {
-      // Code that will run on successful response
-      // from the server.
       console.log(response);
-      // artistFromServer will be an Array of artists
       const playerFromServer = response.data;
       const contentDiv = document.querySelector('#playerTableBody');
-      for (let players of playerFromServer) {
+      for (let player of playerFromServer) {
         contentDiv.innerHTML += `
                   <tr>
-                      <td>${players.answerOne}</td>
-                      <td>${players.answerTwo}</td>
-                      <td>${players.answerThree}</td>
+                      <td>${player.answerOne}</td>
+                      <td>${player.answerTwo}</td>
+                      <td>${player.answerThree}</td>
                   </tr>
               `;
       }
     })
     .catch(function (error) {
-      // Code that will run on any errors from the server.
       console.log(error);
       alert('Something bad happened! Check the console for more details.');
     });
@@ -80,5 +64,6 @@ function onSubmit() {
 
 
 
-onReady()
+
+onReady();
 
