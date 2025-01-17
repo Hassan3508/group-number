7a72abb289760fd58764c2b63c57108188282824
Alexander Ruziska <ziskapants@gmail.com>
2025-01-17 10:49:56 -0600

    got posts from server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,39 @@ function onReady() {
   console.log("JavaScript is loaded!")
 }
 
+
+function fetchGuesses() {
+  //clear old data
+  const contentDiv = document.querySelector('#playerTableBody');
+  contentDiv.innerHTML = '';
+  axios({
+    method: 'GET',
+    url: '/playerdata',
+  })
+    .then(function (response) {
+      // Code that will run on successful response
+      // from the server.
+      console.log(response);
+      // artistFromServer will be an Array of artists
+      const playerFromServer = response.data;
+      const contentDiv = document.querySelector('#playerTableBody');
+      for (let players of playerFromServer) {
+        contentDiv.innerHTML += `
+                  <tr>
+                      <td>${players.answerOne}</td>
+                      <td>${players.answerTwo}</td>
+                      <td>${players.answerThree}</td>
+                  </tr>
+              `;
+      }
+    })
+    .catch(function (error) {
+      // Code that will run on any errors from the server.
+      console.log(error);
+      alert('Something bad happened! Check the console for more details.');
+    });
+}
+
+
 onReady()
+
--- server/server.js
+++ server/server.js
@@ -3,14 +3,34 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5001;
 
+const playerDataArray = [{
+  
+  answerOne: 'Miles Davis',
+  answerTwo: 'Miles Davis',
+  answerThree: 'Street tacos',
+}];
+
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
+app.use(express.json({extended: true}));
+
 
 // GET & POST Routes go here
 
+
+app.get('/playerdata', (req, res) => {
+  res.send(playerDataArray);
+});
+
+app.post('/playerdata', (req, res) => {
+  console.log('Yay we did it!');
+  playerDataArray.push(req.body);
+  res.sendStatus(201);
+});
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
